f3b89b97ebcc69bc624f5acb34b646c2195289e1
hrishikeshpm78 <hrishikesh.pm@coviam.com>
2019-05-30 16:10:13 +0530

    added 3 tables

---
+++ leaderboard/leaderboardBackend/src/main/java/com/coviam/leaderboard/entity/DailyLeaderboard.java
@@ -0,0 +1,55 @@
+package com.coviam.leaderboard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="daily_leaderboard")
+public class DailyLeaderboard {
+    int dayId;
+    String username;
+    int score;
+    int userRank;
+
+    public DailyLeaderboard(int dayId, String username, int score, int userRank) {
+        this.dayId = dayId;
+        this.username = username;
+        this.score = score;
+        this.userRank = userRank;
+    }
+
+    public DailyLeaderboard() {
+    }
+
+    public int getDayId() {
+        return dayId;
+    }
+
+    public void setDayId(int dayId) {
+        this.dayId = dayId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getUserRank() {
+        return userRank;
+    }
+
+    public void setUserRank(int userRank) {
+        this.userRank = userRank;
+    }
+}
