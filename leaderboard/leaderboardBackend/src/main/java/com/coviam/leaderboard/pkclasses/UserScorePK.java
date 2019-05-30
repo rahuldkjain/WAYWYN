0632120e6ed134ba2741f04870df377d1de08442
Rahul Jain <rahuldkjain@gmail.com>
2019-05-30 16:26:21 +0530

    UserScore entity

---
+++ leaderboard/leaderboardBackend/src/main/java/com/coviam/leaderboard/pkclasses/UserScorePK.java
@@ -0,0 +1,30 @@
+package com.coviam.leaderboard.pkclasses;
+
+import java.io.Serializable;
+
+public class UserScorePK implements Serializable {
+    public Integer userId;
+    public Integer contestId;
+
+    public UserScorePK(Integer userId, Integer contestId) {
+        this.userId = userId;
+        this.contestId = contestId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getContestId() {
+        return contestId;
+    }
+
+    public void setContestId(Integer contestId) {
+        this.contestId = contestId;
+    }
+
+}
