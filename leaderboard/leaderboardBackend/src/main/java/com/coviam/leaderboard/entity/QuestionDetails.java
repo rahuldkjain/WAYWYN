c700d366051ddaaade54fb62a94df46f03fb588d
hrishikeshpm78 <hrishikesh.pm@coviam.com>
2019-05-30 16:31:55 +0530

    "added qd and weekly tables"

---
+++ leaderboard/leaderboardBackend/src/main/java/com/coviam/leaderboard/entity/QuestionDetails.java
@@ -0,0 +1,47 @@
+package com.coviam.leaderboard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="question_details")
+public class QuestionDetails {
+    @Id
+    int questionId;
+    String question_text;
+    String answer;
+
+    public QuestionDetails(int questionId, String question_text, String answer) {
+        this.questionId = questionId;
+        this.question_text = question_text;
+        this.answer = answer;
+    }
+
+    public QuestionDetails() {
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestion_text() {
+        return question_text;
+    }
+
+    public void setQuestion_text(String question_text) {
+        this.question_text = question_text;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
